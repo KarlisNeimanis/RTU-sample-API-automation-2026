29deeddb4cffbc2048d6f901f2ced3ea978e8931
karlis neimanis <Karlisneim@gmail.com>
2026-03-28 16:41:35 +0200

    New

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ export const config = {
         status: 'active',
     },
     BOOKS_DEV: {
-        host: 'http//localhost:1010'
+        host: 'https//localhost:1010'
     },
     BOOKS_STG: {
-        host: 'http//localhost:2020'
+        host: 'https//localhost:2020'
     },
     BOOKS_PRD: {
-        host: 'http//localhost:3030'
+        host: 'https//localhost:3030'
     }
 }
 
